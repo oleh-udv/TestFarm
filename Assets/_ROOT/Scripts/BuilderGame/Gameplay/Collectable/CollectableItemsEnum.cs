27eff2a050794f3e2d5613aa30ffa9809e36c475
andrii.baranchuk <andreybaranchuk98@gmail.com>
2023-07-05 16:20:17 +0300

    Changed CollectableItemsEnum to PlantType.

--- Assets/_ROOT/Scripts/BuilderGame/Gameplay/Collectable/CollectableItemsEnum.cs
+++
@@ -1,8 +0,0 @@
-namespace BuilderGame.Gameplay.Collectable
-{
-    public enum CollectableItemsEnum
-    {
-        Tomato,
-        Corn,
-    }
-}
---
+++ Assets/_ROOT/Scripts/BuilderGame/Gameplay/Plants/PlantType.cs
@@ -0,0 +1,9 @@
+namespace BuilderGame.Gameplay.Plants
+{
+    //AndriiCodeReview: Renamed enum from "CollectableItemsEnum"
+    public enum PlantType
+    {
+        Tomato,
+        Corn,
+    }
+}
